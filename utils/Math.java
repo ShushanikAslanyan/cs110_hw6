d01042f7670c64a5b2196534a2c413fe558b7e82
ShushanikAslanyan <sh.ledzeppelin@mail.ru>
2016-11-27 20:04:52 +0400

    Update Math.java

--- utils/Math.java
+++ utils/Math.java
@@ -4,13 +4,13 @@ public class Math {
 	public static int factorial(int n){
 		if(n == 0)
 			return 1;
-		return n*factorial(n-1);
+		return n * factorial(n-1);
 	}
 	
 	public static int factorialLoop(int n) {
 		int result = 1;
-		for(int i = 1; i<=n; i++) {
-			result *= i;
+		for(int i = 1; i <= n; i++) {
+			result * = i;
 		}
 		return result;
 	}
